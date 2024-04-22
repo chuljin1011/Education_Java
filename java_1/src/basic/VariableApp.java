2093c3ea80c1a2616eb8aeae07e0639ada27d3e5
itwill <itwill@DESKTOP-UPNOSB0>
2024-04-22 18:17:19 +0900

    240422

--- java_1/src/basic/VariableApp.java
+++ java_1/src/basic/VariableApp.java
@@ -2,6 +2,17 @@ package basic;
 
 public class VariableApp {
 	public static void main(String[] args) {
+		int su;// 정수값을 저정하기 위한 변수 선언
+		
+		su = 100;
+		
+		System.out.println(su);
+		
+		int num = 200;
+		
+		int ab = su + num;
+		
+		System.out.println("ab = " + ab);
 		
 	}
 
