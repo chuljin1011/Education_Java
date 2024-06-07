87517d34ee22cc6d2df41e6977b27727f01040d5
itwill <itwill@DESKTOP-UPNOSB0>
2024-06-07 11:34:01 +0900

    240607

---
+++ jdbc/src/xyz/itwill/jdbc/StaticBlock.java
@@ -0,0 +1,12 @@
+package xyz.itwill.jdbc;
+
+public class StaticBlock {
+	public StaticBlock() {
+		System.out.println("################ StaticBlock 클래스의 기본 생성자 호출");
+	}
+	
+	public void display() {
+		System.out.println("######## StaticBlock 클래스의 display() 메소드 호출 ##");
+	}
+
+}
