c07d60a1c00eebbef15015b0b450a25abd2ef61d
itwill <itwill@DESKTOP-UPNOSB0>
2024-08-07 18:18:09 +0900

    240807

---
+++ mvc/src/main/java/xyz/itwill/mvc/BoardListModel.java
@@ -0,0 +1,25 @@
+package xyz.itwill.mvc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BoardListModel implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		ActionForward actionForward=new ActionForward();
+		
+		
+		
+	
+		return null;
+	}
+	
+	
+
+}
