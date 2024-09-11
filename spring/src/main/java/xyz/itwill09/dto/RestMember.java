afdec54aca4c71cf8a69539845db440ef06e449a
itwill <itwill@DESKTOP-UPNOSB0>
2024-09-11 17:36:05 +0900

    240911

---
+++ spring/src/main/java/xyz/itwill09/dto/RestMember.java
@@ -0,0 +1,13 @@
+package xyz.itwill09.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RestMember {
+	private String id;
+	private String name;
+	private String email;
+	
+}
